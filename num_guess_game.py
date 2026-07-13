fa63113245a562dd3a0e22f89325ec0d9e8bbbcc
Mohamed Adnan <adnanamadou07@icloud.com>
2026-07-13 01:25:01 +0100

    Added win logic

--- num_guess_game.py
+++ num_guess_game.py
@@ -4,4 +4,8 @@ random_num = random.randint(1 , 9)
 
 attempts = 0
 while attempts < 3:
+    user_num = int(input("Choose a number between 1 & 9: "))
+    if user_num == random_num:
+        print("You guessed the number!")
+        break
     
