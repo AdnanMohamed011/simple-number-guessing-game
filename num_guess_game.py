582daf812b1913b997c00bd6b4a9130d1a3d69c8
Mohamed Adnan <adnanamadou07@icloud.com>
2026-07-13 02:01:38 +0100

    Added score tracking

--- num_guess_game.py
+++ num_guess_game.py
@@ -3,12 +3,23 @@ import random
 random_num = random.randint(1 , 9)
 
 attempts = 0
+score = 0
 while attempts < 3:
     attempts += 1
+
     user_num = int(input("Choose a number between 1 & 9: "))
     if user_num == random_num:
-        print("You guessed the number!")
-        break
+        score += 10
+        print(f"You guessed the number! You got '{score}' points!")
+        choice = input("Would you like to keep playing? (Y/N) ").capitalize()
+
+        if choice == "Y":
+            attempts = 0
+            random_num = random.randint(1 , 9)
+            user_num = int(input("Choose a number between 1 & 9: "))
+
+        else:
+            break
 
     if user_num in range(random_num - 2, random_num + 2):
         print("Hot! You are so close!")
@@ -16,4 +27,4 @@ while attempts < 3:
         print("Cold, not close.")
     
     if attempts == 3 and user_num != random_num:
-        print("You are out of attempts. Game over.")
+        print(f"You are out of attempts. Game over. You got '{score}' Points")
