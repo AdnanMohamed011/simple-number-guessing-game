21a3afd8489d9399780d7e5ca30049ad0d220c9c
Mohamed Adnan <adnanamadou07@icloud.com>
2026-07-13 01:28:40 +0100

    Added attempt logic

--- num_guess_game.py
+++ num_guess_game.py
@@ -4,8 +4,13 @@ random_num = random.randint(1 , 9)
 
 attempts = 0
 while attempts < 3:
+    attempts += 1
     user_num = int(input("Choose a number between 1 & 9: "))
     if user_num == random_num:
         print("You guessed the number!")
         break
-    
+
+    if user_num in range(random_num - 2, random_num + 2):
+        print("Hot! You are so close!")
+    else:
+        print("Cold, not close.")
